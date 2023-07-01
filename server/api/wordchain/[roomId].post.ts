715b4e59d807b38dd890bce14cd79071f3e3b743
Ichirou.S <sey323@users.noreply.github.com>
2023-07-01 23:25:11 +0900

    :sparkles: 最初に指定した回数を超えた時、ステータスを完了にする (#33)

--- server/api/wordchain/[roomId].post.ts
+++ server/api/wordchain/[roomId].post.ts
@@ -1,14 +1,58 @@
 import { createError } from "h3";
 import { v4 as uuidv4 } from "uuid";
 import { PostWordChainRequest, WordChain } from "../../models/wordchain";
-import { addWordChain } from "../../facades/repositories/wordChain";
+import {
+  addWordChain,
+  getWordChains,
+} from "../../facades/repositories/wordChain";
 import { uploadImage } from "../../facades/storage/generatedImage";
 import { editImage } from "../../facades/generativeai/stability";
 import { Buffer } from "buffer";
-import { updateRoomUpdatedAt } from "../../facades/repositories/room";
+import { getRoom, updateRoomAfterInput } from "../../facades/repositories/room";
+import { RoomDomain, RoomStatus } from "../../models/room";
+
+/**
+ * ユーザのしりとりの入力に応じて次の部屋のステータスを検証する
+ */
+async function checkNextRoomStatus(roomId: string, word: string) {
+  const room: RoomDomain | undefined = await getRoom(roomId);
+  const wordChains: WordChain[] | undefined = await getWordChains(roomId);
+  if (room === undefined || wordChains === undefined) {
+    throw new Error("RoomId is not found");
+  }
+
+  if (room.chainCount > wordChains.length + 1) {
+    // まだ規定数のしりとりが行われていない時
+    return RoomStatus.Playing; // 部屋のステータスはゲームが続行中
+  } else {
+    return RoomStatus.Completed; // 部屋のステータスを完了にする
+  }
+}
+
+/**
+ * file画像を変換してstorageにアップロードする
+ */
+async function convertAndUploadImage(file: Buffer, wordChain: WordChain) {
+  const base64Data = file.toString().split(",")[1];
+  const decodedData = Buffer.from(base64Data, "base64");
+
+  const image =
+    process.env.MODE === "development"
+      ? decodedData
+      : await editImage(wordChain.word, decodedData);
+  // fs.writeFileSync("temp.png", decodedData); // デバッグ様にローカルに保存
+  await uploadImage(image, wordChain.wordChainId);
+}
 
 export default defineEventHandler(async (event) => {
   try {
+    if (event === undefined) {
+      return createError({
+        statusCode: 400,
+        statusMessage: "Failed to read body",
+      });
+    }
+
     const data: MultiPartData[] | undefined = await readMultipartFormData(
       event
     );
@@ -34,7 +78,6 @@ export default defineEventHandler(async (event) => {
       }
     }
     const roomId: string | undefined = event.context.params?.roomId;
-
     if (!(requestBody && file && roomId)) {
       return createError({
         statusCode: 400,
@@ -51,9 +94,15 @@ export default defineEventHandler(async (event) => {
       createdAt: createdAt,
     };
 
+    // TODO: 直前の単語と続いていることを確認する
+    const nextRoomStatus: RoomStatus = await checkNextRoomStatus(
+      roomId,
+      requestBody.word
+    );
+
     // GCPに保存する処理
     await addWordChain(wordChain);
-    await updateRoomUpdatedAt(roomId);
+    await updateRoomAfterInput(roomId, nextRoomStatus);
     await convertAndUploadImage(file, wordChain);
 
     return {
@@ -67,18 +116,3 @@ export default defineEventHandler(async (event) => {
     });
   }
 });
-
-/**
- * file画像を変換してstorageにアップロードする
- */
-async function convertAndUploadImage(file: Buffer, wordChain: WordChain) {
-  const base64Data = file.toString().split(",")[1];
-  const decodedData = Buffer.from(base64Data, "base64");
-
-  const image =
-    process.env.MODE === "development"
-      ? decodedData
-      : await editImage(wordChain.word, decodedData);
-  // fs.writeFileSync("temp.png", decodedData); // デバッグ様にローカルに保存
-  await uploadImage(image, wordChain.wordChainId);
-}

5910fe136212d747cec8ac12ef38df48de777686
sayako21 <>
2023-07-08 18:51:43 +0900

    [追加]お絵描き画面のデザイン

--- pages/playroom/[roomId].vue
+++ pages/playroom/[roomId].vue
@@ -4,10 +4,24 @@
   <failure-pop v-show="isFailure" @hiddenPop="hiddenPop" />
   <div class="bg">
     <div>
-      <h1>{{ room.roomPassPhrase }}のしりとり</h1>
-      <h1>目標: {{ room.chainCount }} / のこり {{ remainingChainCount }}回</h1>
-      <h1>始まりの言葉「{{ room.firstWord }}」</h1>
+      <div class="btn-b">
+        <div>
+          <h1 class="text-3xl">{{ room.roomPassPhrase }}’s room</h1>
+          <div class="hukidashi-right">
+            <p>始まりの言葉</p>
+          </div>
+          「{{ room.firstWord }}」
+        </div>
+      </div>
     </div>
+
+    <chain-result-image :roomStatus="room.roomStatus" />
+    <chain-count-goal
+      :chainCount="room.chainCount"
+      :remainingChainRate="remainingChainRate"
+      :length="wordChains.length"
+    />
+
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <PictureList :wordChains="wordChains" />
       <DrawTool v-show="showDrawingArea" @addWordChain="addWordChain" />
@@ -66,6 +80,11 @@ const remainingChainCount = computed(() => {
   return room.value.chainCount - wordChains.value.length;
 });
 
+// 残り必要なしりとりの回数
+const remainingChainRate = computed(() => {
+  return (wordChains.value.length / room.value.chainCount) * 100;
+});
+
 // 登録
 const addWordChain = async (
   postWordChainRequest: PostWordChainRequest,
@@ -111,5 +130,59 @@ const addWordChain = async (
   background-size: cover;
   background-position: center;
   height: 100vh;
+}
+.hukidashi-right {
+  position: relative;
+  display: inline-block;
+  margin: 1em 15px 0 0;
+  padding: 7px 10px;
+  min-width: 150px;
+  max-width: 100%;
+  font-size: 14px;
+  background: #fff;
+  border: solid 3px #555;
+  box-sizing: border-box;
+  text-align: center;
+}
+
+.hukidashi-right:before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  right: -24px;
+  margin-top: -12px;
+  border: 12px solid transparent;
+  border-left: 12px solid #fff;
+  z-index: 2;
+}
+
+.hukidashi-right:after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  right: -30px;
+  margin-top: -14px;
+  border: 14px solid transparent;
+  border-left: 14px solid #555;
+  z-index: 1;
+}
+
+.hukidashi-right p {
+  margin: 0;
+  padding: 0;
+}
+.btn-b {
+  padding: 1rem 1rem;
+  border: 3px solid #212121;
+  border-bottom-width: 8px;
+  border-radius: 10px;
+  color: #212121;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  display: block;
+  width: 350px;
+  margin: 5px auto;
+  background: beige;
 }
 </style>

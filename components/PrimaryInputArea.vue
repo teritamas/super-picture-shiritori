0859f5858e26b19d06395ae3286fd3d290f36b00
sey323 <sey323_nm@yahoo.co.jp>
2023-08-07 23:17:25 +0900

    :recycle: テキストボックスをコンポーネント

---
+++ components/PrimaryInputArea.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="text-center">
+    <div class="speech-balloon-right">
+      <label for="chainCount">{{ label }}</label>
+    </div>
+    <input
+      class="d-input bg-gray-50 border border-gray-900 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      style="border: 2px solid #000"
+      type="text"
+      :value="inputValue"
+      @input="$emit('update:inputValue', $event.target?.value)"
+      :placeholder="placeholder"
+    />
+    <small v-if="informationMessage" class="block">{{
+      informationMessage
+    }}</small>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  inputValue: {
+    type: Object as PropType<string | number>,
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    required: true,
+  },
+  informationMessage: {
+    type: String,
+    required: false,
+  },
+});
+
+defineEmits(["update:inputValue"]);
+</script>
+
+<style>
+.d-input {
+  width: 200px;
+  display: inline;
+}
+
+.speech-balloon-right {
+  position: relative;
+  display: inline-block;
+  margin: 1em 15px 0 0;
+  padding: 7px 10px;
+  min-width: 100px;
+  max-width: 100%;
+  font-size: 14px;
+  background: #fff;
+  border: solid 3px #555;
+  box-sizing: border-box;
+  text-align: center;
+}
+
+.speech-balloon-right:before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  right: -24px;
+  margin-top: -12px;
+  border: 12px solid transparent;
+  border-left: 13px solid #fff;
+  z-index: 2;
+}
+
+.speech-balloon-right:after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  right: -30px;
+  margin-top: -14px;
+  border: 14px solid transparent;
+  border-left: 14px solid #555;
+  z-index: 1;
+}
+</style>
--- pages/makePlayroom.vue
+++ pages/makePlayroom.vue
@@ -2,45 +2,22 @@
   <div class="main">
     <div>
       <h2 class="title mt-12">ルームを作成</h2>
-      <div class="text-center">
-        <div class="hukidashi-right">
-          <label for="roomPassPhrase">目標設定</label>
-        </div>
-        <input
-          class="d-input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          type="text"
-          placeholder="10"
-          style="border: 2px solid #000"
-          v-model="form.chainCount"
-        />
-        <small class="block"
-          >しりとりをつなげる回数を入力してください。例）10</small
-        >
-      </div>
-      <div class="text-center">
-        <div class="hukidashi-right">
-          <label for="createUserId">ユーザ名</label>
-        </div>
-        <input
-          class="d-input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          type="text"
-          style="border: 2px solid #000"
-          v-model="form.createUserId"
-          placeholder="ニックネーム"
-        />
-      </div>
-      <div class="text-center">
-        <div class="hukidashi-right">
-          <label for="chainCount">あいことば</label>
-        </div>
-        <input
-          class="d-input bg-gray-50 border border-gray-900 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          style="border: 2px solid #000"
-          type="text"
-          v-model="form.roomPassPhrase"
-          placeholder="あいことば"
-        />
-      </div>
+      <PrimaryInputArea
+        v-model:inputValue="form.chainCount"
+        :label="'目標設定'"
+        :placeholder="'10'"
+        :informationMessage="'しりとりをつなげる回数を入力してください。例）10'"
+      />
+      <PrimaryInputArea
+        v-model:inputValue="form.createUserId"
+        :label="'ユーザ名'"
+        :placeholder="'ニックネーム'"
+      />
+      <PrimaryInputArea
+        v-model:inputValue="form.roomPassPhrase"
+        :label="'あいことば'"
+        :placeholder="'あいことばを入力'"
+      />
       <div class="text-center">
         <button class="btn-c mt-5" id="back-button" @click="addRoom">
           ルームを作る
@@ -108,47 +85,6 @@ const addRoom = async () => {
   height: 100vh;
   width: 100vw;
 }
-.hukidashi-right {
-  position: relative;
-  display: inline-block;
-  margin: 1em 15px 0 0;
-  padding: 7px 10px;
-  min-width: 100px;
-  max-width: 100%;
-  font-size: 14px;
-  background: #fff;
-  border: solid 3px #555;
-  box-sizing: border-box;
-  text-align: center;
-}
-
-.hukidashi-right:before {
-  content: "";
-  position: absolute;
-  top: 50%;
-  right: -24px;
-  margin-top: -12px;
-  border: 12px solid transparent;
-  border-left: 13px solid #fff;
-  z-index: 2;
-}
-
-.hukidashi-right:after {
-  content: "";
-  position: absolute;
-  top: 50%;
-  right: -30px;
-  margin-top: -14px;
-  border: 14px solid transparent;
-  border-left: 14px solid #555;
-  z-index: 1;
-}
-
-.d-input {
-  width: 200px;
-  display: inline;
-}
-
 .btn-c {
   padding: 1rem 1rem;
   border: 3px solid #212121;
